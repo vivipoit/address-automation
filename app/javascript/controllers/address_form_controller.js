14e84a5c490ce81410d009d4c637c438e1771b14
Vivi Poit <vwpoit@gmail.com>
2022-06-20 20:48:39 +0200

    turn street into dynamically populated selection based on city

--- app/javascript/controllers/address_form_controller.js
+++ app/javascript/controllers/address_form_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="address-form"
 export default class extends Controller {
 
-  static targets = ['city', 'postalCode']
+  static targets = ['city', 'postalCode', 'street']
 
   connect() {
     super.connect()
@@ -16,13 +16,36 @@ export default class extends Controller {
       .then((data) => {
         this.cityTarget.innerHTML = ''
 
+        const option = document.createElement('option')
+        option.innerHTML = '- enter a postal code above -'
+        this.cityTarget.appendChild(option)
+
         data.cities.forEach((city) => {
           if (this.postalCodeTarget.value == '' || this.postalCodeTarget.value == city.postal_code) {
-            const option = document.createElement('option')
-            option.innerHTML = city.name
+            const option = new Option(city.name, city.id)
             this.cityTarget.appendChild(option)
           }
         })
+      })
+  }
+
+  updateStreetOptions() {
+    fetch(`/streets?city_id=${this.cityTarget.value}`)
+      .then(response => response.json())
+      .then((data) => {
+        this.streetTarget.innerHTML = ''
+
+        if (data.streets.length > 0) {
+          data.streets.forEach((street) => {
+            const option = document.createElement('option')
+            option.innerHTML = street.name
+            this.streetTarget.appendChild(option)
+          })
+        } else {
+          const option = document.createElement('option')
+          option.innerHTML = '- no streets -'
+          this.streetTarget.appendChild(option)
+        }
       })
   }
 }
