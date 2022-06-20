608dda0417ce2894f93de48dc6d281660eb0f13d
Vivi Poit <vwpoit@gmail.com>
2022-06-20 21:43:32 +0200

    turn number into a dynamically populated dropdown based on street

--- app/javascript/controllers/address_form_controller.js
+++ app/javascript/controllers/address_form_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="address-form"
 export default class extends Controller {
 
-  static targets = ['city', 'postalCode', 'street']
+  static targets = ['city', 'postalCode', 'street', 'number']
 
   connect() {
     super.connect()
@@ -16,16 +16,14 @@ export default class extends Controller {
       .then((data) => {
         this.cityTarget.innerHTML = ''
 
-        const option = document.createElement('option')
-        option.innerHTML = '- enter a postal code above -'
-        this.cityTarget.appendChild(option)
-
         data.cities.forEach((city) => {
           if (this.postalCodeTarget.value == '' || this.postalCodeTarget.value == city.postal_code) {
             const option = new Option(city.name, city.id)
             this.cityTarget.appendChild(option)
           }
         })
+
+        this.updateStreetOptions()
       })
   }
 
@@ -37,14 +35,35 @@ export default class extends Controller {
 
         if (data.streets.length > 0) {
           data.streets.forEach((street) => {
-            const option = document.createElement('option')
-            option.innerHTML = street.name
+            const option = new Option(street.name, street.id)
             this.streetTarget.appendChild(option)
           })
         } else {
           const option = document.createElement('option')
           option.innerHTML = '- no streets -'
           this.streetTarget.appendChild(option)
+        }
+
+        this.updateNumberOptions()
+      })
+  }
+
+  updateNumberOptions() {
+    fetch(`/street_numbers?street_id=${this.streetTarget.value}`)
+      .then(response => response.json())
+      .then((data) => {
+        this.numberTarget.innerHTML = ''
+
+        if (data.street_numbers.length > 0) {
+          data.street_numbers.forEach((street_number) => {
+            const option = document.createElement('option')
+            option.innerHTML = street_number.number
+            this.numberTarget.appendChild(option)
+          })
+        } else {
+          const option = document.createElement('option')
+          option.innerHTML = '- no numbers -'
+          this.numberTarget.appendChild(option)
         }
       })
   }
